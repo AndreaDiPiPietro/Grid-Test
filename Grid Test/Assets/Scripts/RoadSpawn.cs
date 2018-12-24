caa6c7f4c1126a2796c1d69657f348a518d81c5f
Ninechi <dipietroandrea@hotmail.it>
2018-12-24 18:15:23 +0100

    First commit for testing the grid system in City Simulator by Michelangelo Conserva and Andrea Di Pietro

---
+++ Grid Test/Assets/Scripts/RoadSpawn.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawn : MonoBehaviour {
+
+    public GameObject roadChunk;
+    public Dictionary<int, List<GameObject>> roadDict;
+
+    public int block = 0;
+
+    void Start()
+    {
+        roadDict = new Dictionary<int, List<GameObject>>();
+    }
+
+    public void InitialSpawn()
+    {
+        var cur = Instantiate(roadChunk);
+        cur.GetComponent<GridSnapping>().enabled = true;
+    }
+
+    public void UpdateBlocks()
+    {
+        List<GameObject> curBlock = new List<GameObject>();
+
+        var traces = GameObject.FindGameObjectsWithTag("Trace");
+        foreach (GameObject g in traces)
+        {
+            var curTransform = g.transform;
+            Destroy(g);
+            var curRoadChunk = Instantiate(roadChunk, curTransform.position, curTransform.rotation, this.transform);
+            StartCoroutine(DeleteIfColliding(curRoadChunk, block));
+            curBlock.Add(curRoadChunk);
+        }
+
+        roadDict.Add(block++, curBlock);
+    }
+
+    IEnumerator DeleteIfColliding(GameObject curRoadChunk, int curBlock)
+    {
+        yield return new WaitForFixedUpdate();
+
+        if (curRoadChunk.GetComponent<CollisionChecking>().isColliding)
+        {
+            roadDict[curBlock].Remove(curRoadChunk);
+            Destroy(curRoadChunk);
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
